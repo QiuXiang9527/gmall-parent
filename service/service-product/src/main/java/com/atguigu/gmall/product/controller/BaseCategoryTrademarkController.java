1fc7b6841f84dc9d88c550c42438993c5e800331
QiuXiang9527 <127528688+QiuXiang9527@users.noreply.github.com>
2023-11-15 14:02:32 +0800

    Initial commit

---
+++ service/service-product/src/main/java/com/atguigu/gmall/product/controller/BaseCategoryTrademarkController.java
@@ -0,0 +1,9 @@
+package com.atguigu.gmall.product.controller;/**
+    *@ClassName BaseCategoryTrademarkController
+    *@Description TODO
+    *@Author qiu
+    *@Date2023/11/13 21:59
+    *@Version I.0
+**/
+public class BaseCategoryTrademarkController {
+}
